ed29aee6e9b05a6d94bff996c23eb3c236b0abfa
Ahmed Safaa <ahmed@sz4h.com>
2023-08-02 10:47:53 +0300

    Version up

--- woocommerce-likecard.php
+++ woocommerce-likecard.php
@@ -1,10 +1,10 @@
 <?php
 /**
- * Plugin Name: Space Woocommerce Like4Card Integeration
+ * Plugin Name: Space Woocommerce Like4Card Integration
  * Description:
  * Plugin URI: https://sz4h.com/
  * Author: Ahmed Safaa
- * Version: 1.0.0
+ * Version: 1.0.1
  * Author URI: https://sz4h.com/
  *
  * Text Domain: space-woocommerce-likecard
